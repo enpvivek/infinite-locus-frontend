7bc87d21f4c5c1f46994423b8249be7c53bb0ac4
Rohit Kumar <kumar25rohit08@gmail.com>
2024-01-06 16:12:22 +0530

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from "react";
-import "./App.css";
-import Products from "./Products";
-import "./index.css";
-import Login from "./Login";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Products from './Products';
+import Header from './Header';
+import Sales from './Sales';
+import Items from './Items';
+import AddProducts from './AddProducts';
+
 
 export default function App() {
   return (
-    <>
-      <Products />
-    </>
+    <Router>
+     <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+             
+              <Products />
+            </>
+          }
+        />
+        <Route path="/sales" element={<Sales />} />
+        <Route path="/items" element={<Items />} />
+        <Route path="/add-products" element={<AddProducts />} />
+      </Routes>
+    </Router>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,27 +1,21 @@
-import React from "react";
-const User = "User";
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 const Header = () => {
   return (
-    <div className="flex flex-row h-auto w-full  justify-around  m-0 mb-4">
-      <div id="logo" className="flex">
-        <img
-          src="https://static.wixstatic.com/media/4006d0_a651b03946a645eba283b75ec2c6f613~mv2.png/v1/fill/w_186,h_61,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/inifnite_locus_logo_v1_edited.png"
-          alt="Infinite Locus"
-        />
-      </div>
+    <div className="flex flex-row h-auto w-full justify-around m-0 mb-4">
+      {/* ... other code ... */}
       <div id="navbar" className="flex flex-row text-2xl font-semibold gap-x-5">
-        <div>Products</div>
-        <div>Add Product</div>
-        <div>Sales</div>
-        <div>Suggestions</div>
+        <Link to="/">Products</Link>
+        <Link to="/add-products">Add Product</Link>
+        <Link to="/sales">Sales</Link>
+        <Link to="/items">Suggestions</Link>
       </div>
-      <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Logout
-        </button>
-      </div>
+      {/* ... other code ... */}
     </div>
   );
 };
+
 
 export default Header;
