fe8bd5d1421e5916ddd6d12fc1bc2453e80f8ff5
enpvivek <enpvivek@gmail.com>
2024-01-06 14:29:35 +0530

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import Products from './Products'
-import './index.css'
+import { useState } from "react";
+import "./App.css";
+import Products from "./Products";
+import "./index.css";
+import Login from "./Login";
 
 export default function App() {
   return (
     <>
-    <Products/>
+      <Products />
     </>
-    
-  )
+  );
 }
-
-
--- src/Products.jsx
+++ src/Products.jsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Items from "./Items";
 import AddProducts from "./AddProducts";
 import Sales from "./Sales";
+import Login from "./Login";
 
 const Products = () => {
   return (
@@ -10,16 +11,17 @@ const Products = () => {
       {/* Show Admin Name */}
       <div id="products" className="flex flex-col w-full h-screen mx-0">
         <Header />
+        {/* <Login /> */}
         <Sales />
         {/* <AddProducts /> */}
-        {/* <div id="itemContainer" className="flex">
-          <div id="productContainer" className=" grid grid-cols-2 w-full gap-5">
+        <div id="itemContainer" className="flex">
+          <div id="productContainer" className=" grid grid-cols-3 w-full gap-5">
             <Items />
             <Items />
             <Items />
             <Items />
           </div>
-        </div> */}
+        </div>
       </div>
     </>
   );
